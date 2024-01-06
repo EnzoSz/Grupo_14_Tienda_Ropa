32dfa960dae2a981cbc09b50cbf365989a203529
Enzo Soliz <enzosoliz95@gmail.com>
2024-01-06 19:07:55 -0300

    Creando ruta para productos, Actualizando estilos de detalle de productos.

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const productsFilePath = path.join(__dirname, '../../public/assets/data/products.json');
+const productsFilePath = path.join(__dirname, '../database/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const productsController = {
     index: (req, res) => {
