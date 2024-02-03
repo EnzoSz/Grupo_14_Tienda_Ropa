46a89e59b7dca1325e2416b85ec11707930516de
Enzo Soliz <enzosoliz95@gmail.com>
2024-02-03 14:37:39 -0300

    tarea 4 y 5 del sprint 5 terminadas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,11 @@
+//requerimos el bcrypt
+const bcrypt = require("bcryptjs");
+//requerimos el fs
+const fs = require("fs");
 //requerimos path para poder enviar archivos
 const path = require("path");
-
+//obtenemos el archivo JSON
+const usersFilePath = path.join(__dirname, "../database/users.json");
 //creamos el objeto controller
 const userController = {
   index: (req, res) => {
@@ -10,6 +15,38 @@ const userController = {
     res.render("register");
   },
   processRegister: (req, res) => {
+    //leemos el json
+    let users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+    //creamos un objeto literal y guardamos dentro de el la info que viene del form
+    if (req.file) {
+      
+      const newUser = {
+        id: users[users.length - 1].id + 1,
+        nombre: req.body.name,
+        apellido: req.body.lastname,
+        nombreUsuario: req.body.nickname,
+        email: req.body.email,
+        fechaNacimiento: req.body.birthdate,
+        domicilio: req.body.domicilio,
+        password: bcrypt.hashSync(req.body.password, 10),
+        foto: req.file.filename,
+      };
+      //pusheamos el objeto literal al array
+      users.push(newUser);
+      //sobreescribomos el archivo JSON
+      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+      res.redirect('/');
+      console.log(newUser);
+    } else {
+      res.send("error");
+    }
+    //mostramos al usuario un vista
+    // res.redirect("/users");
+  },
+  logout: (req, res) => {
+    res.render("logout");
+  },
+  processLogout: (req, res) => {
     res.send(req.body);
   },
   login: (req, res) => {
