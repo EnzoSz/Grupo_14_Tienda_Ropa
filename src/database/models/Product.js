1a34d926ffd7be6f95fc476e25f8daf69ef1fc78
Danilo Barbosa <dbarbosaf92@gmail.com>
2024-02-29 23:06:02 -0300

    agregando el edit del CRUD con la DB

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -16,7 +16,6 @@ module.exports = (sequelize, DataTypes) => {
     },
     brand_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
     },
     category_id: {
       type: DataTypes.INTEGER,
