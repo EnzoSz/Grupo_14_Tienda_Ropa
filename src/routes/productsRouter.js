edf78a6551870796e9990cf757b07d76d91edde1
Enzo Soliz <enzosoliz95@gmail.com>
2024-03-26 00:31:16 -0300

    UPDATE view uploadProduct

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -20,19 +20,19 @@ const upload = multer({storage})
 
 
 
-//Mostramos todos los productos
-router.get("/:category?", productsController.index);
 //Mostramos un producto
 router.get("/detail/:id", productsController.detail);
 //Creando ruta de categoria
 /* router.get('/hombre', productsController.hombre);
 router.get('/mujer', productsController.mujer);
 router.get('/kids', productsController.kids);
- */
+*/
 //Cargar un producto
 router.get("/create", productsController.create);
 router.post("/create", upload.single('image_product'),createMiddleware, productsController.processCreate);
 
+//Mostramos todos los productos
+router.get("/:category?", productsController.index);
 //Editamos un producto
 router.get("/edit/:id", productsController.editProduct);
 router.put("/edit/:id", upload.single('image_product'),createMiddleware, productsController.processEdit);
