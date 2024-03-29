edfb423dbb6fb492d0b0b6e4900c63503bb54f07
SPARDAK117 <oswaldo.mejiac@gmail.com>
2024-03-29 15:38:28 -0600

    Cambio en userLoggedMiddleware y hago checkpoint de mi rama antes de hacer merge

--- Grupo_14_Tienda_Ropa-oswaldo/src/middlewares/userLoggedMiddleware.js
+++ Grupo_14_Tienda_Ropa-oswaldo/src/middlewares/userLoggedMiddleware.js
@@ -7,24 +7,24 @@ const db = require("../database/models");
 
 async function userLoggedMiddleware(req, res, next) {
   res.locals.isLogged = false;
-  //obtnemos los usuarios de la base de datos
-  let usersDB = await db.User.findAll();
-  //obtemos el email de la cookie
-  let emailInCookie = req.cookies.userEmail;
-  //verificamos que el email de la cookie exista en la base de datos
-  let userWithEmail = usersDB.find((user) => user.dataValues.email === emailInCookie);
-  // console.log(userWithEmail);
-  //verificamos que el email de la cookie exista en el json
-  /*let userWithEmail = users.find((user) => user.email === emailInCookie);
-  */
-  if (userWithEmail) {
-    req.session.userLogged = userWithEmail;
+    //obtnemos los usuarios de la base de datos
+    let usersDB = await db.User.findAll();
+    //obtemos el email de la cookie
+    let emailInCookie = req.cookies.userEmail;
+    //verificamos que el email de la cookie exista en la base de datos
+    let userWithEmail = usersDB.find((user) => user.dataValues.email === emailInCookie);
+    // console.log(userWithEmail);
+    //verificamos que el email de la cookie exista en el json
+    /*let userWithEmail = users.find((user) => user.email === emailInCookie);
+    */
+    if (userWithEmail) {
+      req.session.userLogged = userWithEmail;
+    }
+    if (req.session && req.session.userLogged) {
+      res.locals.isLogged = true;
+      //pasamos el usuario que viene de la sesion a una variable local
+      res.locals.userLogged = req.session.userLogged;
+    } 
+    next();
   }
-  if (req.session && req.session.userLogged) {
-    res.locals.isLogged = true;
-    //pasamos el usuario que viene de la sesion a una variable local
-    res.locals.userLogged = req.session.userLogged;
-  } 
-  next();
-}
-module.exports = userLoggedMiddleware;
+  module.exports = userLoggedMiddleware;
