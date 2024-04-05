b2af9a740830b2a341df5dc5c354a6908df22f61
Danilo Barbosa <dbarbosaf92@gmail.com>
2024-04-04 22:51:03 -0300

    agregando error 404vista

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -28,35 +28,6 @@ async function userLoggedMiddleware(req,res,next) {
     next();
 }
 module.exports = userLoggedMiddleware;
- /*  try {
-     // Inicializa las variables locales para indicar si el usuario está logueado y si es un administrador
-     res.locals.isLogged = false;
-     res.locals.isAdmin = false;
-     // Intenta obtener el valor de la cookie 'userCookie'
-     const userCookie = req.cookies.userCookie;
-      // Verifica si el usuario es un administrador basado en la sesión
-     if (req.session.admin) {
-       res.locals.isAdmin = true; // Si el usuario es un administrador, establece la variable local 'isAdmin' en verdadero
-       res.locals.admin = req.session.admin; // Asigna el valor de la sesión 'admin' a la variable local 'admin'
-     }
-      // Verifica si existe una cookie 'userCookie'
-     if (userCookie) {
-       req.session.userLogged = userCookie; // Si existe, asigna el valor de la cookie a la sesión 'userLogged'
-     }
-     // Verifica si el usuario está logueado basado en la sesión
-     if (req.session.userLogged) {
-       res.locals.isLogged = true; // Si el usuario está logueado, establece la variable local 'isLogged' en verdadero
-       res.locals.userLogged = req.session.userLogged;  // Asigna el valor de la sesión 'userLogged' a la variable local 'userLogged'
-     }
-     // Llama a la función 'next' para pasar el control al siguiente middleware o controlador
-     next();
-  } catch (error) {
-     // Aquí puedes manejar el error, por ejemplo, registrándolo y enviando una respuesta de error
-     console.error(error);
-     res.status(500).send('Error interno del servidor');
-  } */
- 
-
 
  
 
