36650f0662659b5704c94ac4a9a7d70232940780
Enzo Soliz <enzosoliz95@gmail.com>
2024-04-08 22:59:44 -0300

    update database config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,19 +2,19 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "homies",
-    "password": "homies2024",
-    "database": "db_homies",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "migrationStorage": "sequelize",
     "migrationStorageTableName": "migrations"
   },
   "test": {
-    "username": "homies",
-    "password": "homies2024",
-    "database": "db_homies",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
